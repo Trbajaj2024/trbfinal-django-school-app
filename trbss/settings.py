df7ede37d2f3dcf7673674413d693e308a125a8d
akanksha <akankshachoudhary0207@gmail.com>
2026-03-12 01:09:23 +0530

    fix static files

--- trbss/settings.py
+++ trbss/settings.py
@@ -132,10 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files (User uploaded files)
 MEDIA_URL = '/media/'
